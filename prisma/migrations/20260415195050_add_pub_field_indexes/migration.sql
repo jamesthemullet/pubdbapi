0a00b8d388e20ed3335fd0a5693b920fe01d26d9
James Winfield <hello@jameswinfield.co.uk>
2026-04-16 08:01:42 +0100

    add migration

---
+++ prisma/migrations/20260415195050_add_pub_field_indexes/migration.sql
@@ -0,0 +1,17 @@
+-- CreateIndex
+CREATE INDEX "Pub_city_idx" ON "Pub"("city");
+
+-- CreateIndex
+CREATE INDEX "Pub_operator_idx" ON "Pub"("operator");
+
+-- CreateIndex
+CREATE INDEX "Pub_borough_idx" ON "Pub"("borough");
+
+-- CreateIndex
+CREATE INDEX "Pub_postcode_idx" ON "Pub"("postcode");
+
+-- CreateIndex
+CREATE INDEX "Pub_area_idx" ON "Pub"("area");
+
+-- CreateIndex
+CREATE INDEX "Pub_country_idx" ON "Pub"("country");
